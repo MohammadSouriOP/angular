d5e2561850afee9a4ec22b45df3d7bd815a5ec59
Mohammad Souri <mohammad.souri@sociumtech.com>
2025-04-06 17:20:34 +0300

    last

--- ng-tutorial/src/app/app.component.ts
+++ ng-tutorial/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RolesComponent } from './components/roles/roles.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RolesComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'ng-tutorial';
